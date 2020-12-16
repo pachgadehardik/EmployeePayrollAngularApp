79b5d199e8e527cad205fdefda0daeccf53c3127
pachgadehardik <pachgadehardik@gmail.com>
2020-12-16 09:14:35 +0530

    Updated getEmployee component

--- src/component/get-employee/get-employee.component.ts
+++ src/component/get-employee/get-employee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { EmployeeService } from 'src/service/employee.service';
 
 @Component({
@@ -10,8 +11,10 @@ export class GetEmployeeComponent implements OnInit {
 
   employee = [];
   
+  
   constructor(
-    private employeeService: EmployeeService
+    private employeeService: EmployeeService,
+    private router: Router,
   ) { }
 
   ngOnInit(): void {
@@ -22,13 +25,50 @@ export class GetEmployeeComponent implements OnInit {
     this.employeeService.getData()
       .subscribe(response => {
         console.log("response " + JSON.stringify(response))
-        this.employee = response.empList;
+        this.employee = response.obj;
       },
         error => {
           console.log(error);
         }
       )
   }
+
+  addEmployee(){
+    this.router.navigate(['add'])
+  }
+
+  update(node){
+    console.log(node.id)
+    this.employeeService.getById(node.id).subscribe(response =>{
+       response.obj
+      console.log("inside")
+      this.router.navigate(['add'], response.obj)
+    }),
+    error =>{
+      console.log(error)
+    }
+    
+  }
+
+  delete(node){
+    console.log(node.id)
+    this.employeeService.delete(node.id).subscribe(response => {
+      console.log(JSON.stringify(response))
+      alert("Employee SuccessFully Deleted!")
+      this.ngOnInit();
+    }),
+    error =>{
+    console.log(error);
+    }
+  }
+
+  logout(){
+    localStorage.removeItem('token');
+    console.log("Logout Called")
+    this.router.navigate(['']);
+  }
+
+
 
   
 
